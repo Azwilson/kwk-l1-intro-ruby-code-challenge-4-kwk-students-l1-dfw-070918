17688b04f22d4c9c4a29e550a9ce38a55919b127
Azwilson <azya.w99@gmail.com>
2018-07-12 14:17:46 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -5,11 +5,8 @@ least_coins_hash = {:quarters => 25, :dimes => 10, :nickels => 5, :pennies =>
 
 
 def least_coins(coins)
- cents = {:quarters => 25, :dimes => 10, :nickels => 5, :pennies => 
-1}
- 
- 
- 
+ cents = {:quarters => 0, :dimes => 0, :nickels => 0, :pennies => 
+0}
  
  while coins >= 25
  least_coins_hash[:quarters] += 1
