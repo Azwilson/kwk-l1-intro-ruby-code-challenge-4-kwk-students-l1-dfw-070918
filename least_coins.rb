8258f46a36f07f2e09b1d6a44a1791651442baf6
Azwilson <azya.w99@gmail.com>
2018-07-12 14:13:32 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -10,22 +10,22 @@ def least_coins(25)
  
  
  while coins >= 25
- least_coins_hash[:quarters]+= 1
+ least_coins_hash[:quarters] += 1
  coins = coins-25
  end
  
  while coins >= 10
- least_coins_hash[:dimes]+= 1 
+ least_coins_hash[:dimes] += 1 
  coins = coins-10
  end
 
- while coins >=
-  least_coins_hash[:nickels]
+ while coins >= 5
+  least_coins_hash[:nickels] += 1
   coins = coins-5 
  end
 
  while coins >= 1
- least_coins_hash[:pennies]+= 1 
+ least_coins_hash[:pennies] += 1 
  coins = coins-1
  end 
 end
